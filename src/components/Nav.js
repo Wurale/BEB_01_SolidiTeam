996f3c9a4f354752f5ef9baa42b03fb46322f85d
Parkstelth <stelthicmaster@gmail.com>
2021-12-20 16:53:41 +0900

    change footer css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -43,27 +43,6 @@ function Nav({ setmainaccount, setmainweb3, login }) {
 
   return (
     <div>
-      {/* <div className="menu">
-        <div className="dropdown-container">
-          <div>
-            <img src="https://storage.googleapis.com/opensea-static/Logomark/OpenSea-Full-Logo%20(dark).png" />
-          </div>
-          <div>
-            <button>
-              <FiChevronDown className="down-icon" />
-            </button>
-          </div>
-        </div>
-        <div className="search-container">
-          <div className="icon">
-            <MdSearch className="search-icon" />
-          </div>
-          <input
-            className="search-input"
-            placeholder="Search items, collections, and accounts"
-          />
-        </div>
-      </div> */}
       <div className="menu-large">
         <div className="dropdown-container">
           <div>
